9da1fdea1782233d37877a8a378063b3b5a5002c
Omar Attya Pero <58884931+omar95-pero@users.noreply.github.com>
2021-02-06 09:01:51 +0200

    Create First View

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace Controllers;
 
 use framework\Controller;
+use framework\Views;
 
 class  HomeController extends Controller
 {
     public function index()
     {
-        echo 'Welcome To Controller And Index Function';
+        return Views::make('home', ['title' => 'Title Of Home Page', 'message' => 'Welcome To Home Page']);
     }
 }
--- framework/Views.php
+++ framework/Views.php
@@ -4,10 +4,11 @@ namespace framework;
 
 class Views
 {
-    protected $path_folder = 'Views';
-    private $ext = '.tpl.php';
+    protected static $path_folder = 'Views';
+    private static $ext = '.tpl.php';
     public static function make($path_view, $args = [])
     {
-        include $this->path_folder . '/' . $path_view . $this->ext;
+        extract($args);
+        include self::$path_folder . '/' . $path_view . self::$ext;
     }
 }
